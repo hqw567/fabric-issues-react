6a023e48b307d74bfa2a0a318fc783a948343067
Herway <hqw6@qq.com>
2024-08-12 11:22:16 +0800

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,17 @@ const App = () => {
   useEffect(() => {
     if (!canvasRef.current) return
     const canvas = new fabric.Canvas(canvasRef.current)
-
     dataJson.objects.forEach((item) => {
-      // issues 1: When I have a custom attribute with "type" in it, I get an error `Error: fabric: No class registered for any`
+      // issues 1: When I have a custom attribute with "type" in it, I get an error `Error: TypeError: Cannot read properties of undefined (reading 'split') at _Io._splitTextIntoLines `
       //
       // Comment the following code to see the error
-      delete item.myData.type
+      // delete item.myData.type
+    })
+    canvas.loadFromJSON(dataJson).then(() => {
+      canvas.requestRenderAll()
     })
 
-    const controller = new AbortController()
-    canvas
-      .loadFromJSON(dataJson, undefined, { signal: controller.signal })
-      .then(() => {
-        canvas.requestRenderAll()
-      })
-      .catch((e) => {
-        console.error(e.message ?? 'aborted')
-      })
     return () => {
-      controller.abort()
       canvas.dispose()
     }
   }, [])
@@ -40,3 +32,18 @@ const App = () => {
 }
 
 export default App
+
+export function loadFont(fontName, fontUrl) {
+  return new Promise((resolve, reject) => {
+    const font = new FontFace(fontName, `url(${fontUrl})`)
+    font
+      .load()
+      .then((loadedFont) => {
+        document.fonts.add(loadedFont)
+        resolve(loadedFont)
+      })
+      .catch((error) => {
+        reject(new Error(`Font loading failure ${error}`))
+      })
+  })
+}
